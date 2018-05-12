79925a075bbca488d9b32ad6adeebe4a8cbd8168
Emi-C <emil.costanzo@gmail.com>
2018-05-12 14:22:04 +0200

    imperative getValueByPath, recursive fallback if no provided sortField for nested data structures, tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import moment from "moment";
-const DEFAULTS = { sortDir: "ASC", sortField: null, throwError: false };
+const DEFAULTS = { sortDir: "ASC", sortObjectField: null, throwError: false };
 
 const getOptions = options => {
   return {
@@ -30,23 +30,30 @@ const desc = (a, b) => {
   if (a === b) return 0;
 };
 
-const getPropertyByPath = (propertyName, object) => {
+const getValueByPath = (propertyName, object) => {
   const parts = propertyName.split(".");
-  let property = object;
-
-  for (let i = 0; i < parts.length; i++) {
-    property = property[parts[i]];
-  }
-
-  return property;
+  return parts.reduce((acc, curr) => {
+    return acc[curr];
+  }, object);
 };
 
-const compare = (a, b, sortDir, sortField, throwError) => {
+const fallbackSortObjectField = item => {
+  let sortObjectField = Array.isArray(item) ? "0" : Object.keys(item)[0];
+  if (typeof item[sortObjectField] === "object") {
+    sortObjectField += `.${fallbackSortObjectField(item[sortObjectField])}`;
+  }
+  return sortObjectField;
+};
+
+const compare = (a, b, sortDir, sortObjectField, throwError) => {
+  //gets types
   const typeA = getType(a);
   const typeB = getType(b);
+  //clones parameters for manipulation
   let itemA = a;
   let itemB = b;
 
+  //if we have different types in the list, error is thrown
   if (typeA !== typeB || Array.isArray(itemA) !== Array.isArray(itemB)) {
     if (throwError) {
       throw new Error("Different types cannot be compared");
@@ -54,43 +61,52 @@ const compare = (a, b, sortDir, sortField, throwError) => {
     return 0;
   }
 
+  //array of objects or array of arrays
   if (typeA === "object") {
-    if (!sortField) {
-      sortField = Array.isArray(itemA) ? "0" : Object.keys(itemA)[0];
+    //fallback for missing sortObjectField
+    if (!sortObjectField) {
+      sortObjectField = fallbackSortObjectField(itemA);
     }
+    //given the path in sortObjectField (path.to.key) gets the correct value to compare
+    itemA = getValueByPath(sortObjectField, a);
+    itemB = getValueByPath(sortObjectField, b);
 
-    itemA = getPropertyByPath(sortField, a);
-    itemB = getPropertyByPath(sortField, b);
-
+    //throws error for undefined values
     if (itemA === undefined) {
       if (throwError) {
-        throw new Error(`Specified sortField has not been found in item ${a}`);
+        throw new Error(
+          `Specified sortObjectField has not been found in item ${a}`
+        );
       }
       return 0;
     }
-
     if (itemB === undefined) {
       if (throwError) {
-        throw new Error(`Specified sortField has not been found in item ${b}`);
+        throw new Error(
+          `Specified sortObjectField has not been found in item ${b}`
+        );
       }
       return 0;
     }
   }
 
+  //sort function
   return sortDir === "DESC" ? desc(itemA, itemB) : asc(itemA, itemB);
 };
 
 /** A function to sort an array in a type-aware mode
  * @param {Array.<any>} items  - Array to sort. The items have to be of the same type
- * @param {Object} options - Include options parameters to be used
- * @param {string} options.sortDir  - Direction to sort to. It can be 'ASC' or 'DESC'
- * @param {string} options.sortField - In case of objects array, the field to use to sort
- * @param {boolean} options.handleError - If set to true in case of error returns the array sorted by the 0th element (or field in case of objects)
+ * @param [{Object}] options - Include options parameters to be used
+ * @param {string='ASC'} options.sortDir  - Direction to sort to. It can be 'ASC' or 'DESC'
+ * @param {string||Array.strings=null} options.sortObjectField - In case of objects array, the field to use to sort, if null fallsback to first key in object
+ * @param {boolean=true} options.throwError - If set to true in case of error returns the error, otherwise returns the array in the original order
  * @return {Array.<any>} - The sorted array
  */
 const sort = (items, options) => {
-  const { sortDir, sortField, throwError } = getOptions(options);
-  return items.sort((a, b) => compare(a, b, sortDir, sortField, throwError));
+  const { sortDir, sortObjectField, throwError } = getOptions(options);
+  return items.sort((a, b) =>
+    compare(a, b, sortDir, sortObjectField, throwError)
+  );
 };
 
 export default sort;
--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,31 @@
 import sort from "../src";
 
-const items = [{ name: "luca" }, { name: "andrea" }, { name: "mario" }];
+const items = [
+  { name: { text: "luca", value: 9172 } },
+  { name: { text: "andrea", value: 346 } },
+  { name: { text: "mario", value: 3 } }
+];
 
-test("sort ascending", () => {
+test("sort default values -> sortObjectField fallback", () => {
   expect(sort(items)).toEqual([
-    { name: "andrea" },
-    { name: "luca" },
-    { name: "mario" }
+    { name: { text: "andrea", value: 346 } },
+    { name: { text: "luca", value: 9172 } },
+    { name: { text: "mario", value: 3 } }
   ]);
 });
 
-test("sort descending", () => {
+test("sort descending, tests default values with direction", () => {
   expect(sort(items, { sortDir: "DESC" })).toEqual([
-    { name: "mario" },
-    { name: "luca" },
-    { name: "andrea" }
+    { name: { text: "mario", value: 3 } },
+    { name: { text: "luca", value: 9172 } },
+    { name: { text: "andrea", value: 346 } }
+  ]);
+});
+
+test("sort by nested value, tests iteration for sortObjectFields", () => {
+  expect(sort(items, { sortObjectField: "name.value" })).toEqual([
+    { name: { text: "mario", value: 3 } },
+    { name: { text: "andrea", value: 346 } },
+    { name: { text: "luca", value: 9172 } }
   ]);
 });
