40a042b2d925433951bafffc5298a068306a6ffa
Emi-C <emil.costanzo@gmail.com>
2018-05-14 19:01:06 +0200

    handles same values preserve order, handles arrays for sortBy multiple parameters

--- test/index.test.js
+++ test/index.test.js
@@ -3,29 +3,49 @@ import sort from "../src";
 const items = [
   { name: { text: "luca", value: 9172 } },
   { name: { text: "andrea", value: 346 } },
-  { name: { text: "mario", value: 3 } }
+  { name: { text: "mario", value: 346 } },
+  { name: { text: "paolo", value: 1346 } },
+  { name: { text: "andrea", value: 1246 } }
 ];
 
-test("sort default values -> sortObjectField fallback", () => {
+test("sort default values -> sortBy fallback", () => {
   expect(sort(items)).toEqual([
     { name: { text: "andrea", value: 346 } },
+    { name: { text: "andrea", value: 1246 } },
     { name: { text: "luca", value: 9172 } },
-    { name: { text: "mario", value: 3 } }
+    { name: { text: "mario", value: 346 } },
+    { name: { text: "paolo", value: 1346 } }
   ]);
 });
 
 test("sort descending, tests default values with direction", () => {
   expect(sort(items, { sortDir: "DESC" })).toEqual([
-    { name: { text: "mario", value: 3 } },
+    { name: { text: "paolo", value: 1346 } },
+    { name: { text: "mario", value: 346 } },
     { name: { text: "luca", value: 9172 } },
+    { name: { text: "andrea", value: 1246 } },
     { name: { text: "andrea", value: 346 } }
   ]);
 });
 
-test("sort by nested value, tests iteration for sortObjectFields", () => {
-  expect(sort(items, { sortObjectField: "name.value" })).toEqual([
-    { name: { text: "mario", value: 3 } },
+test("sort by nested value, tests iteration for sortBy", () => {
+  expect(sort(items, { sortBy: "name.value" })).toEqual([
     { name: { text: "andrea", value: 346 } },
+    { name: { text: "mario", value: 346 } },
+    { name: { text: "andrea", value: 1246 } },
+    { name: { text: "paolo", value: 1346 } },
     { name: { text: "luca", value: 9172 } }
+  ]);
+});
+
+test("sort by multiple nested values, tests iteration and reducer for sortBy, in descendent order", () => {
+  expect(
+    sort(items, { sortDir: "DESC", sortBy: ["name.text", "name.value"] })
+  ).toEqual([
+    { name: { text: "paolo", value: 1346 } },
+    { name: { text: "mario", value: 346 } },
+    { name: { text: "luca", value: 9172 } },
+    { name: { text: "andrea", value: 1246 } },
+    { name: { text: "andrea", value: 346 } }
   ]);
 });
